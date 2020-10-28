2c583c25d50dcbc87abc150a991907bbbddd0890
Chitipolu Sri Sudheera <csrisudheera96@gmail.com>
2020-10-28 14:10:54 -0500

    added name -sudheera

--- AndriodProj/app/src/main/java/com/example/andriodapp/MainActivity.java
+++ AndriodProj/app/src/main/java/com/example/andriodapp/MainActivity.java
@@ -20,6 +20,7 @@ public class MainActivity extends AppCompatActivity {
             public void onClick(View view) {
                 startActivity(new Intent(MainActivity.this,MainActivity.class));
                 // sai
+                //sudheera
             }
         });
 
