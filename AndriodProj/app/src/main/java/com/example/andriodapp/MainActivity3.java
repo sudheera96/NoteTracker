f0775c23c32465b044a5515c7bd45cff0e02259b
Chitipolu Sri Sudheera <csrisudheera96@gmail.com>
2020-10-28 15:08:30 -0500

    added main activity 3 - sudheera

---
+++ AndriodProj/app/src/main/java/com/example/andriodapp/MainActivity3.java
@@ -0,0 +1,25 @@
+package com.example.andriodapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity3 extends AppCompatActivity {
+    private Button homeButton;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main3);
+        homeButton = findViewById(R.id.homeButton);
+        homeButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(MainActivity3.this,MainActivity.class));
+            }
+        });
+    }
+}
