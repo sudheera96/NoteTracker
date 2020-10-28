7f96a96f47295800ed11ade8de1bf61fa3bc8acb
SaiKrishna1545 <s538340@nwmissouri.edu>
2020-10-28 13:03:41 -0500

    added main activity java file - Saikrishna

--- AndriodProj/app/src/main/java/com/example/andriodapp/MainActivity.java
+++ AndriodProj/app/src/main/java/com/example/andriodapp/MainActivity.java
@@ -2,13 +2,25 @@ package com.example.andriodapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
+    private Button NEXT;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        NEXT = findViewById(R.id.button3);
+        NEXT.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(MainActivity.this,MainActivity.class));
+            }
+        });
+
     }
 }
