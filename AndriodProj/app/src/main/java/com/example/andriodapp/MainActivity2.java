d832b7d969a157e18e7c8087130713e61e764095
SaiKrishna1545 <s538340@nwmissouri.edu>
2020-10-29 16:11:21 -0500

    updated calender view in MainActivity2.java - Saikrishna

--- AndriodProj/app/src/main/java/com/example/andriodapp/MainActivity2.java
+++ AndriodProj/app/src/main/java/com/example/andriodapp/MainActivity2.java
@@ -17,7 +17,8 @@ public class MainActivity2 extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main2);
         NEXT1 = findViewById(R.id.button4);
-
+        CalendarView calendar_view = (CalendarView) findViewById(R.id.calendarView);
+       
 
         NEXT1.setOnClickListener(new View.OnClickListener() {
             @Override
