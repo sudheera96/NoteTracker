a452eb7c3a83d5b5d0f58e1b09c34be3bf550730
SaiKrishna1545 <s538340@nwmissouri.edu>
2020-10-29 16:12:57 -0500

    adjusted minimum data as todays date in calendar view in MainActivity2.java - Saikrishna

--- AndriodProj/app/src/main/java/com/example/andriodapp/MainActivity2.java
+++ AndriodProj/app/src/main/java/com/example/andriodapp/MainActivity2.java
@@ -18,7 +18,7 @@ public class MainActivity2 extends AppCompatActivity {
         setContentView(R.layout.activity_main2);
         NEXT1 = findViewById(R.id.button4);
         CalendarView calendar_view = (CalendarView) findViewById(R.id.calendarView);
-       
+        calendar_view.setMinDate((new Date().getTime()));
 
         NEXT1.setOnClickListener(new View.OnClickListener() {
             @Override
