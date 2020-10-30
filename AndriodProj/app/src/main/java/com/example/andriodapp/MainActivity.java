b2d6ab90d8421e2cd1bfa2e27edd49906dde456a
Rajeshwari_Rudra <rajrudra.raj@gmail.com>
2020-10-30 11:47:48 -0400

    added new button functionality

--- AndriodProj/app/src/main/java/com/example/andriodapp/MainActivity.java
+++ AndriodProj/app/src/main/java/com/example/andriodapp/MainActivity.java
@@ -41,6 +41,14 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                addItem(view);
+            }
+        });
+
+
 
     }
 }
