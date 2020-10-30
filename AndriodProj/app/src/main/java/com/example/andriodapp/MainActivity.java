8d54b45dd1245a3f84cafae94340c4d4cae36712
Rajeshwari_Rudra <rajrudra.raj@gmail.com>
2020-10-30 11:41:16 -0400

    declared buttons and text views - Rajeshwari

--- AndriodProj/app/src/main/java/com/example/andriodapp/MainActivity.java
+++ AndriodProj/app/src/main/java/com/example/andriodapp/MainActivity.java
@@ -2,28 +2,33 @@ package com.example.andriodapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ListView;
+import android.widget.Toast;
+
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
 
-    private Button NEXT;
+    private ArrayList<String> items;
+    private ArrayAdapter<String> itemsAdapter;
+    private ListView listView;
+    private Button button;
+    private Button button3;
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        NEXT = findViewById(R.id.button3);
-        NEXT.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                startActivity(new Intent(MainActivity.this,MainActivity2.class));
-                // sai
-                //sudheera
-                //harika
-            }
-        });
+
 
     }
 }
