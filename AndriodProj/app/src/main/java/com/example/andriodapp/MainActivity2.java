067a5fcef8b0e272f46620c062f2fc7db9b7e40a
Chitipolu Sri Sudheera <csrisudheera96@gmail.com>
2020-10-30 15:59:29 -0500

    updated activity 2 both xml and java files - Sri Sudheera

--- AndriodProj/app/src/main/java/com/example/andriodapp/MainActivity2.java
+++ AndriodProj/app/src/main/java/com/example/andriodapp/MainActivity2.java
@@ -13,11 +13,13 @@ import java.util.Date;
 
 public class MainActivity2 extends AppCompatActivity {
     private Button NEXT1;
+    private Button NEXT2;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main2);
         NEXT1 = findViewById(R.id.button4);
+        NEXT2 = findViewById(R.id.TIMER);
         CalendarView calendar_view = (CalendarView) findViewById(R.id.calendarView);
         calendar_view.setMinDate((new Date().getTime()));
 
@@ -25,6 +27,12 @@ public class MainActivity2 extends AppCompatActivity {
             @Override
             public void onClick(View view) {
                 startActivity(new Intent(MainActivity2.this,MainActivity3.class));
+            }
+        });
+        NEXT2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(MainActivity2.this,MainActivity4.class));
             }
         });
     }
