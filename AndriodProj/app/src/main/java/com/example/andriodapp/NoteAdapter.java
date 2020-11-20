06cac18ec7205b41320d64ec3d8ab61ce2b71796
Rajeshwari_Rudra <rajrudra.raj@gmail.com>
2020-11-19 19:44:36 -0500

    add NoteAdapter.java - Rajeshwari

---
+++ AndriodProj/app/src/main/java/com/example/andriodapp/NoteAdapter.java
@@ -0,0 +1,25 @@
+package com.example.andriodapp;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.TextView;
+
+import java.util.ArrayList;
+
+public class NoteAdapter extends ArrayAdapter<Note> {
+    public NoteAdapter(Context context, ArrayList<Note> users) {
+        super(context, 0, users);
+    }
+
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent) {
+        // Get the data item for this position
+       
+
+        // Return the completed view to render on screen
+        return convertView;
+    }
+}
