bcbcbe918fca9ffc1397c02568dcde435612061c
Chitipolu Sri Sudheera <csrisudheera96@gmail.com>
2020-11-19 18:19:17 -0600

    added intent, item delete, on click and saved data to db for MainActivity.java - Sudheera

--- AndriodProj/app/src/main/java/com/example/andriodapp/MainActivity.java
+++ AndriodProj/app/src/main/java/com/example/andriodapp/MainActivity.java
@@ -48,3 +48,82 @@ public class MainActivity extends AppCompatActivity {
         Collections.sort(noteArrayList);
         noteAdapter.notifyDataSetChanged();
     }
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        listView = findViewById(R.id.listView);
+        noteArrayList = NotesList.getSingleton().getNotesList();
+        noteAdapter = new NoteAdapter(this, noteArrayList);
+        listView.setAdapter(noteAdapter);
+
+        rootNode = FirebaseDatabase.getInstance();
+        Query myMostViewedPostsQuery = rootNode.getReference();
+
+        myMostViewedPostsQuery.addValueEventListener(new ValueEventListener() {
+            @RequiresApi(api = Build.VERSION_CODES.N)
+            public void onDataChange(DataSnapshot dataSnapshot) {
+//                noteAdapter.clear();
+                noteArrayList.clear();
+                NotesList notesList = FirebaseUtil.deserialize(dataSnapshot, NotesList.class);
+                if(notesList!=null && notesList.getNotesList() != null && !notesList.getNotesList().isEmpty()){
+                    notesList.getNotesList().stream().forEach(note -> {
+                        noteArrayList.add(note);
+                    });
+                }
+                Collections.sort(noteArrayList);
+                noteAdapter.notifyDataSetChanged();
+
+            }
+
+            @Override
+            public void onCancelled(DatabaseError databaseError) {
+                System.out.println("The read failed: " + databaseError.getCode());
+            }
+        });
+
+        listView.setOnItemLongClickListener(new AdapterView.OnItemLongClickListener() {
+            @Override
+            public boolean onItemLongClick(AdapterView<?> adapterView, View view, int i, long l) {
+                Context context = getApplicationContext();
+                // removing each task from to-do list
+                // adding a feedback message as a pop-up it an itemis deleted.
+                Toast.makeText(context, "Item in list is removed", Toast.LENGTH_LONG).show();
+                noteArrayList.remove(i);
+                noteAdapter.notifyDataSetChanged();
+
+                saveToDB();
+                return true;
+            }
+        });
+
+        button = findViewById(R.id.button);
+        button.setOnClickListener(view -> {
+            EditText input = findViewById(R.id.edittext);
+            String itemText = input.getText().toString();
+
+            if (!(itemText.equals(""))) {
+                // adding each task to to-do list
+                Note note = new Note();
+                note.setId((long) (noteArrayList.size() + 1));
+                note.setNote(itemText);
+                noteAdapter.add(note);
+
+                saveToDB();
+                input.setText("");
+            } else {
+                // adding a feedback message as a alert if user tries to add an empty text
+                Toast.makeText(getApplicationContext(), "Please enter text in list", Toast.LENGTH_LONG).show();
+            }
+
+        });
+
+        listView.setOnItemClickListener((parent, view, position, id) -> {
+            Intent intent = new Intent(MainActivity.this, MainActivity2.class);
+            intent.putExtra("positionOfClickValue", position);
+            startActivity(intent);
+        });
+
+
+    }
